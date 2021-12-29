e79b41eab6282c9767f82ae4dc73bd331c51926c
葛新 <gexin@gexindeMacBook-Air.local>
2021-12-29 21:01:28 +0800

    modified:   controller/UserController.go 	modified:   go.mod 	modified:   routes.go 	modified:   testLogin1

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,13 +2,15 @@ package controller
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 	"testLogin1/common"
 	"testLogin1/model"
 	"testLogin1/util"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(ctx *gin.Context) {
@@ -50,15 +52,23 @@ func Register(ctx *gin.Context) {
 		return
 	}
 
+	// 密码加密
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+		return
+	}
+
 	// 创建用户
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hashPassword),
 	}
 	DB.Create(&newUser)
 	// 返回结果
 	ctx.JSON(200, gin.H{
+		"code":    200,
 		"message": "注册成功",
 	})
 }
@@ -71,4 +81,44 @@ func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	}
 
 	return false
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+	// 获取参数
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	// 数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		return
+	}
+
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码过短"})
+		return
+	}
+
+	// 判断用户名密码
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+
+	// 判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+	// 发放token
+	token := "123456789"
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功！",
+	})
+	//  返回
 }
